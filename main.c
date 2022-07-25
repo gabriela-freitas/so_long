1f1a41c52e89f5d994e36fd701b5014e1b6e399e
Gabriela Freitas <gabriela2001freitas@gmail.com>
2022-07-25 23:31:09 +0100

    now its just fix leaks and submit uhuu

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gafreita <gafreita@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 20:53:50 by gafreita          #+#    #+#             */
-/*   Updated: 2022/07/25 23:01:09 by gafreita         ###   ########.fr       */
+/*   Updated: 2022/07/25 23:29:46 by gafreita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,28 @@ int	key_code(int keycode, void *param)
 		game_over("Thank you for playing");
 	}
 	if (move_player(keycode))
+	{
 		ft_printf("steps: %d\n", ++game()->steps);
+	}
 	return (1);
 }
 
 int	print_player(void *param)
 {
+	char *str;
+
 	print_map(-1, -1);
-	//print_collectables();
 	mlx_put_image_to_window(infos()->mlx, infos()->win_mlx,
 		img()->player, game()->player.x * PX, game()->player.y * PX);
+	str = ft_strjoin("Steps: ", ft_itoa(game()->steps));
+	mlx_string_put(infos()->mlx, infos()->win_mlx, 32, 20, 0x00FFFFFF, str);
+	free(str);
+}
+
+int	close_win(void *param)
+{
+	mlx_destroy_window(infos()->mlx, infos()->win_mlx);
+	game_over("Thank you for playing");
 }
 
 int	main(int argc, char **argv)
@@ -49,6 +61,7 @@ int	main(int argc, char **argv)
 		(infos()->win_mlx) = mlx_new_window(infos()->mlx,
 				PX * (infos()->width - 1), PX * infos()->height, "so_long");
 		mlx_hook(infos()->win_mlx, 2, 1L << 0, key_code, infos());
+		mlx_hook(infos()->win_mlx, 17, 1L << 2, close_win, infos());
 		open_images();
 		mlx_loop_hook(infos()->mlx, print_player, (void *)infos());
 		mlx_loop(infos()->mlx);
